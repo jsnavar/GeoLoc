75a2eace0d11efccf3b34ceebd491ad34eb5eb8c
Juan Navarro <juansnn@pm.me>
2023-07-27 20:27:36 +0000

    akka-http interface

--- geoloc/build.sbt
+++ geoloc/build.sbt
@@ -10,10 +10,16 @@ lazy val root = (project in file("."))
       "org.apache.spark" %% "spark-sql" % "3.4.1",
       "org.postgresql" % "postgresql" % "42.1.1",
 
+      "com.typesafe.akka" %% "akka-actor-typed" % "2.7.0",
+      "com.typesafe.akka" %% "akka-stream" % "2.7.0",
+      "com.typesafe.akka" %% "akka-http" % "10.5.2",
+      "com.typesafe.akka" %% "akka-http-spray-json" % "10.5.2",
+
       "org.scalacheck" %% "scalacheck" % "1.14.1" % Test,
       "org.scalameta" %% "munit" % "0.7.29" % Test),
     javaOptions ++= Seq("--add-exports=java.base/sun.nio.ch=ALL-UNNAMED"),
     scalacOptions ++= Seq("-language:implicitConversions", "-deprecation"),
     fork := true,
-    run / connectInput := true
+    run / connectInput := true,
+    dependencyOverrides += "org.scala-lang.modules" %% "scala-parser-combinators" % "2.1.1"
   )
--- geoloc/src/main/scala/Main.scala
+++ geoloc/src/main/scala/Main.scala
@@ -1,12 +1,45 @@
+import scala.concurrent.{ExecutionContext, Future}
 import scala.io.StdIn
+
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import spray.json.DefaultJsonProtocol._
+import spray.json.RootJsonFormat
 
 import org.apache.spark.sql.SparkSession
 
 import geoloc.SparkLoc
+import geoloc.geometry.Point
+
+class AkkaServer(locator: SparkLoc, host: String, port: Int) {
+  implicit val system: ActorSystem[_] = ActorSystem(Behaviors.empty, "GeoLoc")
+  implicit val executionContext: ExecutionContext = system.executionContext
+
+  val route: Route = get {
+    pathPrefix("locate" / DoubleNumber / DoubleNumber) { (lon, lat) =>
+      val countriesFuture: Future[Set[String]] = Future {
+        locator.locate(Point(lon.toFloat, lat.toFloat))
+      }
+
+      onSuccess(countriesFuture) { countries =>
+        if(countries.isEmpty) complete(StatusCodes.NotFound)
+        else complete(countries)
+      }
+    }
+  }
+
+  val bindingFuture = Http().newServerAt(host, port).bind(route)
+
+  def stop() = bindingFuture.flatMap(_.unbind()).onComplete(_ => system.terminate())
+}
 
 object Main extends App {
-  import geoloc.geometry.Point
-
   val spark: SparkSession =
     SparkSession
       .builder()
@@ -18,7 +51,8 @@ object Main extends App {
 
   val locator = new SparkLoc(spark, Secret.pgData)
 
-  locator.locate(Point(0f, 0f))
+  val httpServer = new AkkaServer(locator, "localhost", 8080)
+
   println("running")
 
   val mainIterator = Iterator.continually(StdIn.readLine())
@@ -30,5 +64,8 @@ object Main extends App {
   for (point <- mainIterator) {
     println(locator.locate(point))
   }
+
+  httpServer.stop()
+
   spark.close()
 }
